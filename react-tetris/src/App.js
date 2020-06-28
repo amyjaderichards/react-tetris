76d8705672fea1def5384b7943abb0691075e54a
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-28 23:42:17 +0100

    Stage update and player movement finished

--- react-tetris/src/App.js
+++ react-tetris/src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello Amy</h1>
-    </div>
-  );
-}
+import Tetris from "./components/Tetris";
 
+const App = () => (
+  <div className="App">
+    <Tetris />
+  </div>
+);
 
 export default App;
